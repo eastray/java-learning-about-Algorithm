d32d2f404de24b7cc59c8cd3948191a1f6b8a0f5
donghwi <eastray@hanbat.ac.kr>
2017-10-26 22:02:25 +0900

    modified

--- Permutation/src/permutation/Permutation.java
+++ Permutation/src/permutation/Permutation.java
@@ -5,13 +5,17 @@ public class Permutation {
 	public static int[] result = new int[10];
 	public static int[] data = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public void swap(int i, int j) {
+		int temp = data[i];
+		data[i] = data[j];
+		data[j] = temp;
+	}
 
-		Permutation perm = new Permutation();
+	public void process(int q) {
+		for (int i = q - 1; i >= 0; i--)
+			System.out.print(result[i]);
 
-		perm.permutation(4, 3, 3);
-
+		System.out.println();
 	}
 
 	public void permutation(int n, int r, int q) {
@@ -22,31 +26,22 @@ public class Permutation {
 		}
 
 		for (int i = n - 1; i >= 0; i--) {
-
 			swap(i, n - 1);
-//			System.out.println("i: " + i);
-//			System.out.println("n: " + (n - 1));
-//			System.out.println("r: " + r);
+			
 			result[r - 1] = data[n - 1];
+			
 			permutation(n - 1, r - 1, q);
 			swap(i, n - 1);
-			//System.out.println("f");
 		}
 
 	}
 
-	public void process(int q) {
-		for (int i = q - 1; i >= 0; i--)
-			System.out.print(result[i]);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-		System.out.println();
+		Permutation perm = new Permutation();
+
+		perm.permutation(4, 3, 3);
+
 	}
-
-	public void swap(int i, int j) {
-
-		int temp = data[i];
-		data[i] = data[j];
-		data[j] = temp;
-	}
-
 }
--- RedundantPermutation/src/redundantPermutation/RedundantPermutation.java
+++ RedundantPermutation/src/redundantPermutation/RedundantPermutation.java
@@ -4,40 +4,10 @@ public class RedundantPermutation {
 	public static int[] result = new int[10];
 	public static int[] data = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		RedundantPermutation rePerm = new RedundantPermutation();
-		
-		rePerm.redundantPermutation(4, 3, 3);
-		
-	}
-
-	public void redundantPermutation(int n, int r, int q) {
-
-//		for (int i = 0; i < data.length; i++)
-//			System.out.print(data[i]);
-//		System.out.println();
-//
-//		System.out.println("n: " + n + ", r: " + r);
-
-		if (r == 0) {
-			process(q);
-			return;
-		}
-
-		for (int i = n - 1; i >= 0; i--) {
-			//System.out.println("i: " + i);
-			swap(i, n - 1);
-
-			result[r - 1] = data[n - 1];
-
-			redundantPermutation(n, r - 1, q);
-
-			System.out.println("i: " + i + ", n-1: " + (n-1));
-			swap(i, n - 1);
-		}
-
+	public void swap(int i, int j) {
+		int temp = data[i];
+		data[i] = data[j];
+		data[j] = temp;
 	}
 
 	public void process(int q) {
@@ -48,13 +18,31 @@ public class RedundantPermutation {
 		System.out.println();
 	}
 
-	public void swap(int i, int j) {
+	public void redundantPermutation(int n, int r, int q) {
 
-		int temp = data[i];
-		data[i] = data[j];
-		data[j] = temp;
+		if (r == 0) {
+			process(q);
+			return;
+		}
 
-		System.out.println("data[i]: " + data[i]);
-		System.out.println("data[j]: " + data[j]);
+		for (int i = n - 1; i >= 0; i--) {
+			swap(i, n - 1);
+
+			result[r - 1] = data[n - 1];
+
+			redundantPermutation(n, r - 1, q);
+
+			System.out.println("i: " + i + ", n-1: " + (n - 1));
+			swap(i, n - 1);
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		RedundantPermutation rePerm = new RedundantPermutation();
+
+		rePerm.redundantPermutation(4, 3, 3);
 	}
 }
